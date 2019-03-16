90c8b98cc07a293a9a4fde6d3916314cbeb4e75d
asbl <andreas.siebelqit-teaching.de>
2019-03-16 10:09:56 +0100

    Removing all files in .gitignore

--- source/build/lib/gamegridp/gamegrid_clock.py
+++
@@ -1,8 +0,0 @@
-from gamegridp import gamegrid
-from gamegridp import gamegrid_mastergrid
-import pygame
-
-class Clock:
-    def __init__(self, grid):
-        self._grid = grid
-        self._start = grid.counter
